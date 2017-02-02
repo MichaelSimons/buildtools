f2b05ecbb358ec4185afdb6ee39d836e08179500
MichaelSimons <msimons@microsoft.com>
2017-02-02 15:22:52 -0600

    edits

--- src/Dockerfiles/ImageBuilder/ImageBuilder.cs
+++ src/Dockerfiles/ImageBuilder/ImageBuilder.cs
@@ -3,25 +3,48 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace ImageBuilder
 {
     public class ImageBuilder
     {
-        static void Main(string[] args)
-        {
-            // Get the information about the Dockerfiles to build
-            Dictionary<string, ImageInfo> images = Directory.GetFiles(Config.BuildRoot, "Dockerfile", SearchOption.AllDirectories)
-                .Select(dockerfilePath => ImageInfo.Create(dockerfilePath))
-                .ToDictionary(info => info.Name);
+        private static bool s_isPushEnabled = false;
 
+        public static int Main(string[] args)
+        {
+            try
+            {
+                ParseArgs(args);
+
+                // Get the information about the Dockerfiles to build
+                Dictionary<string, ImageInfo> images = Directory.GetFiles(Config.BuildRoot, "Dockerfile", SearchOption.AllDirectories)
+                    .Select(dockerfilePath => ImageInfo.Create(dockerfilePath))
+                    .ToDictionary(info => info.Name);
+
+                BuildImages(images);
+                PushImages(images.Values);
+
+                Console.WriteLine($"{Environment.NewLine}IMAGES BUILT");
+                foreach (ImageInfo imageInfo in images.Values)
+                {
+                    Console.WriteLine(imageInfo.FullName);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static void BuildImages(Dictionary<string, ImageInfo> images)
+        {
             // Build the images in the correct dependency order
-            StringBuilder buildSummary = new StringBuilder($"{Environment.NewLine}IMAGES BUILT{Environment.NewLine}");
             foreach (ImageInfo imageInfo in images.Values.TopologicalSort((info) => GetDependencies(info, images)))
             {
                 Console.WriteLine($"---- Building {imageInfo.FullName} ----");
-                buildSummary.AppendLine(imageInfo.FullName);
 
                 string dockerfile = File.ReadAllText(imageInfo.DockerfilePath);
                 if (imageInfo.HasRepoDependency)
@@ -30,13 +53,11 @@ namespace ImageBuilder
                     dockerfile = ImageInfo.FromRegex.Replace(dockerfile, $"FROM {images[imageInfo.FromImage].FullName}");
                 }
 
-                ImageBuilder.Build(dockerfile, imageInfo);
+                ImageBuilder.BuildImage(dockerfile, imageInfo);
             }
-
-            Console.WriteLine(buildSummary);
         }
 
-        private static void Build(string dockerfile, ImageInfo imageInfo)
+        private static void BuildImage(string dockerfile, ImageInfo imageInfo)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo("docker", $"build -t {imageInfo.FullName} -");
             startInfo.UseShellExecute = false;
@@ -65,6 +86,40 @@ namespace ImageBuilder
             }
 
             return Enumerable.Empty<ImageInfo>();
+        }
+
+        private static void ParseArgs(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (string.Equals(arg, "--Push", StringComparison.OrdinalIgnoreCase))
+                {
+                    s_isPushEnabled = true;
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Unrecognized argument '{arg}'");
+                }
+            }
+        }
+
+        private static void PushImages(IEnumerable<ImageInfo> images)
+        {
+            if (s_isPushEnabled)
+            {
+                foreach (ImageInfo imageInfo in images)
+                {
+                    Console.WriteLine($"---- Pushing {imageInfo.FullName} ----");
+
+                    Process pushProcess = Process.Start("docker", $"push {imageInfo.FullName}");
+                    pushProcess.WaitForExit();
+
+                    if (pushProcess.ExitCode != 0)
+                    {
+                        throw new InvalidOperationException($"Failed to push {imageInfo.DockerfilePath}");
+                    }
+                }
+            }
         }
     }
 }
